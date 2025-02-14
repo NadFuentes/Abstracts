2ca25dae37ee57990047c8917cca04b72373264b
Chung-Un <chungunyum23@gmail.com>
2025-02-14 15:56:26 -0600

    juegoAhorado fijo y juegoAhorcado azar

--- JuegoAhorcadoBase/src/JuegoAhorcadoAzar.java
+++ JuegoAhorcadoBase/src/JuegoAhorcadoAzar.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,48 @@
  *
  * @author chung
  */
-public class JuegoAhorcadoAzar extends JuegoAhorcadosBase{
+public class JuegoAhorcadoAzar extends JuegoAhorcadoBase{
+    String palabrasPosibles[] = {"perro", "gato", "caminadora", "ingenieria", "sistemas", "abstracto"};
+    Random random = new Random();
+    
+    public JuegoAhorcadoAzar(){
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+    palabraSecreta = palabrasPosibles[random.nextInt(palabrasPosibles.length)];
+    palabraActual = new StringBuilder("_".repeat(palabraSecreta.length())); 
+    intentos = 6; 
+    }
+
+    @Override
+    public void jugar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected void actualizarPalabraActual(char letra) {
+        for(int i=0; i<palabraSecreta.length(); i ++){
+            if(verificarLetra(letra) && palabraSecreta.charAt(i)== letra){
+            palabraActual.setCharAt(i, letra);
+                
+            }
+        }    }
+
+    @Override
+    protected boolean verificarLetra(char letra) {
+    for(int i=0; i<palabraSecreta.length();i++){
+            if(letra == palabraSecreta.charAt(i)){
+            return true;
+            }
+        }
+        return false;    }
+
+    @Override
+    protected boolean hasGanado() {
+    if (palabraActual.toString().equals(palabraSecreta)){
+        return true;
+        }
+        return false;    }
     
 }
--- JuegoAhorcadoBase/src/JuegoAhorcadoFijo.java
+++ JuegoAhorcadoBase/src/JuegoAhorcadoFijo.java
@@ -1,3 +1,6 @@
+
+import java.util.Set;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,9 +10,11 @@
  *
  * @author chung
  */
-public class JuegoAhorcadoFijo extends JuegoAhorcadosBase{
-    public JuegoAhorcadoFijo(String palabraSecreta){
-    this.palabraSecreta = palabraSecreta;
+public class JuegoAhorcadoFijo extends JuegoAhorcadoBase{
+    private final String palabraSecretaFija;
+    
+    public JuegoAhorcadoFijo(String palabraSecretaFija){
+    this.palabraSecretaFija = palabraSecretaFija.toUpperCase();
     }
     
     @Override
@@ -20,19 +25,18 @@ public class JuegoAhorcadoFijo extends JuegoAhorcadosBase{
 
     @Override
     protected void actualizarPalabraActual(char letra) {
-            String letraString = Character.toString(letra);
             for(int i=0; i<palabraSecreta.length(); i ++){
-            if(verificarLetra){
-            palabraSecreta.charAt(i)= letraString;
+            if(verificarLetra(letra) && palabraSecreta.charAt(i)== letra){
+            palabraActual.setCharAt(i, letra);
+                
             }
         }
     }
 
     @Override
     protected boolean verificarLetra(char letra) {
-        String letraString = Character.toString(letra);
         for(int i=0; i<palabraSecreta.length();i++){
-            if(letraString.equals(palabraSecreta.charAt(i))){
+            if(letra == palabraSecreta.charAt(i)){
             return true;
             }
         }
@@ -41,10 +45,17 @@ public class JuegoAhorcadoFijo extends JuegoAhorcadosBase{
 
     @Override
     protected boolean hasGanado() {
-        if (palabraActual.equals(palabraSecreta)){
+        if (palabraActual.toString().equals(palabraSecreta)){
         return true;
         }
         return false;
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+        this.palabraSecreta= palabraSecretaFija;
+        this.palabraActual = new StringBuilder("_".repeat(palabraSecreta.length()));
+        this.intentos= 6;
     }
     
 }
