0d0166aeefb471b2dd258d8062e59985159c6939
NadFuentes <nadfuentes03@unitec.edu>
2025-02-14 14:23:40 -0600

    Laboratorio

--- JuegoAhorcadoBase/src/JuegoAhorcado.java
+++ JuegoAhorcadoBase/src/JuegoAhorcado.java
@@ -1,22 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 
 /**
  *
  * @author Nadiesda Fuentes
  */
-public interface JuegoAhorcado{
+public interface JuegoAhorcado {
     void inicializarPalabraSecreta();
     void jugar();
-}
-public abstract class JuegoAhorcadosBase implements JuegoAhorcados{
-    protected String palabraSecreta;
-    protected StringBuilder palabraActual;
-    protected int instentos;
-    
-    public JuegosAhorcadosBase(){
-        this.intentos = 5;
-    }
 }
